c85ba41ad80cbd30801c6e56eee248c953d6a750
Mohamed Shahin <ms4211913@gmail.com>
2024-08-04 20:52:38 +0300

    Initial commit

---
+++ OddEvenNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] Numbers = {0,1, 2, 3, 4, 5, 6, 7,8,9,10};
+        PrintNumbers("Numbers", Numbers);
+        PrintNumbers("Even Numbers",Numbers.Where(n => IsEven(n)));
+        PrintNumbers("Odd Numbers",Numbers.Where((n) => IsOdd(n)));
+    }
+
+    static void PrintNumbers(string title, IEnumerable<int> Numbers)
+    {
+        Console.Write($" {title} : [");
+            foreach (var n in Numbers) {
+            Console.Write($" {n} ,");
+        }
+            Console.Write($"]");
+        Console.WriteLine();
+
+    }
+
+    static bool IsEven(int n)
+    {
+        return n % 2 == 0;
+    }
+
+    static bool IsOdd(int n)
+    {
+        return n % 2 == 1;
+    }
+}
